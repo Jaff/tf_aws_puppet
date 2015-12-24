ce5cc9e5f65c2f1df874594f6eced4a4045f7f27
Mark Jaffe <mark.jaffe@rovicorp.com>
2015-12-23 16:57:31 -0800

    Update some variables

--- master/variables.tf
+++ master/variables.tf
@@ -1,9 +1,15 @@
-variable "region" {}
+variable "region" {
+  default = "${config.region}"
+}
 variable "instance_type" {
   default = "t2.micro"
 }
-variable "subnet_id" {}
-variable "aws_key_name" {}
+variable "subnet_id" {
+  default = "subnet-a47e25d3"
+}
+variable "aws_key_name" {
+  default = "${var.aws_key_name}"
+}
 variable "iam_instance_profile" {
   default = "describe-instances"
 }
